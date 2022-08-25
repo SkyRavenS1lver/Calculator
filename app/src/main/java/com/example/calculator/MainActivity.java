6b0b1ac84ec0acf364ab84848c45e8c9e5e329e0
Raven <revandra.aryo.dwi.krisnandaru@mail.ugm..ac.id>
2022-08-25 21:38:37 +0700

    -second commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     Button subs;
     Button times;
     Button div;
-    String numb1,numb2;
+
     TextView results;
 
     @Override
@@ -32,58 +32,51 @@ public class MainActivity extends AppCompatActivity {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numb1 = display1.getText().toString();
-                numb2 = display2.getText().toString();
-                if (numb1.equals("")){display1.setText("0"); numb1 = "0";}
-                if (numb2.equals("")){display2.setText("0"); numb2 = "0";}
+
                 try {
-                    results.setText(added(Integer.parseInt(numb1),Integer.parseInt(numb2))+"");
+                    results.setText(added(Integer.parseInt(autofilled(display1)),Integer.parseInt(autofilled(display2)))+"");
                 }
                 catch (NumberFormatException e){
-                    results.setText(added(Double.parseDouble(numb1),Double.parseDouble(numb2))+"");
+                    results.setText(added(Double.parseDouble(autofilled(display1)),Double.parseDouble(autofilled(display2)))+"");
                 }
             }
         });
         subs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numb1 = display1.getText().toString();
-                numb2 = display2.getText().toString();
-                if (numb1.equals("")){display1.setText("0"); numb1 = "0";}
-                if (numb2.equals("")){display2.setText("0"); numb2 = "0";}
+
                 try {
-                    results.setText(subs(Integer.parseInt(numb1),Integer.parseInt(numb2))+"");
+                    results.setText(subs(Integer.parseInt(autofilled(display1)),Integer.parseInt(autofilled(display2)))+"");
                 }
                 catch (NumberFormatException e){
-                    results.setText(subs(Double.parseDouble(numb1),Double.parseDouble(numb2))+"");
+                    results.setText(subs(Double.parseDouble(autofilled(display1)),Double.parseDouble(autofilled(display2)))+"");
                 }
                 }
         });
         times.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numb1 = display1.getText().toString();
-                numb2 = display2.getText().toString();
-                if (numb1.equals("")){display1.setText("0"); numb1 = "0";}
-                if (numb2.equals("")){display2.setText("0"); numb2 = "0";}
+
                 try {
-                    results.setText(times(Integer.parseInt(numb1),Integer.parseInt(numb2))+"");
+                    results.setText(times(Integer.parseInt(autofilled(display1)),Integer.parseInt(autofilled(display2)))+"");
                 }
                 catch (NumberFormatException e){
-                    results.setText(times(Double.parseDouble(numb1),Double.parseDouble(numb2))+"");
+                    results.setText(times(Double.parseDouble(autofilled(display1)),Double.parseDouble(autofilled(display2)))+"");
                 }
             }
         });
         div.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                numb1 = display1.getText().toString();
-                numb2 = display2.getText().toString();
-                if (numb1.equals("")){display1.setText("0"); numb1 = "0";}
-                if (numb2.equals("")){display2.setText("0"); numb2 = "0";}
-                results.setText(divide(Double.parseDouble(numb1),Double.parseDouble(numb2))+"");
+
+                results.setText(divide(Double.parseDouble(autofilled(display1)),Double.parseDouble(autofilled(display2)))+"");
             }
         });
+    }
+    public String autofilled(EditText disp1){
+        String numb1 = disp1.getText().toString();
+        if (numb1.equals("")){disp1.setText("0"); return "0";}
+        else {return numb1;}
     }
     public double added(double x, double y)
     {return x+y;}
